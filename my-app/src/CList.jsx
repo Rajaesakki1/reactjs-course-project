84bcf3d3a6c76cc9753b2aa5f2f8c89b6d676fd4
Rajaesakki1 <rajaesakki1806@gmail.com>
2025-06-30 13:21:47 +0530

    delete using use state

--- my-app/src/CList.jsx
+++ my-app/src/CList.jsx
@@ -1,52 +1,62 @@
-import Course from "./course"
-import HTML2 from './assets/HTML2.avif'
-import CSS1 from './assets/CSS1.jpg'
+import Course from "./course";
+import HTML2 from './assets/HTML2.avif';
+import CSS1 from './assets/CSS1.jpg';
+import { useState } from "react";
 
+function CList() {
 
+  const [courses, setCourses] = useState([
+    {
+      id: 1,
+      name: "HTML",
+      price: 774,
+      rating: 5,
+      image: HTML2
+    },
+    {
+      id: 2,
+      name: "CSS",
+      price: 775,
+      rating: 5,
+      image: CSS1
+    },
+    {
+      id: 3,
+      name: "CSS",
+      price: 765,
+      rating: 4,
+      image: CSS1
+    }
+  ]);
 
+  function handleDelete(id) {
+    const newCourse = courses.filter((course) => course.id !== id);
+    setCourses(newCourse);
+    console.log("deleted", id);
+  }
 
+  courses.sort((x, y) => y.rating - x.rating);
 
-function CList(){
-    const courses=[
-        {
-             name:"HTML",
-            price:774,
-            rating:5,
-            image:HTML2
-           
-        },
-         {
-            name:"CSS",
-            price:775,
-            rating:5,
-            image:CSS1
-        },
-         {
-            name:"CSS",
-            price:765,
-            rating:4,
-            image:CSS1
-        }
+  const fcourse = courses.filter((course) => course.price < 800);
+  console.log(fcourse); // this is likely line 34
 
-    ]
-  courses.sort((x,y)=>y.rating -x.rating)
-  const fcourse = courses.filter((course)=> course.price<800)
-const coursesList=fcourse.map((coursess,index)=> (
-<Course 
-key={index}
-name={coursess.name} 
-price={coursess.price}
- rating={coursess.rating} 
- image={coursess.image}
- />
-))
-return(
+  const coursesList = fcourse.map((coursess) => (
+    <Course
+      key={coursess.id}
+      id={coursess.id}
+      name={coursess.name}
+      price={coursess.price}
+      rating={coursess.rating}
+      image={coursess.image}
+      delete={handleDelete}
+    />
+  ));
+
+  return (
     <>
-    {coursesList}
+      {coursesList}
     </>
-);
-
-
-    
+  );
 }
+
 export default CList;
--- my-app/src/Course.jsx
+++ my-app/src/Course.jsx
@@ -1,31 +1,36 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import { useState } from 'react';
-function Course(props){
 
-   // let purchased = false;
-    const [purchased, setPurchased]= useState (false);
-    function BUYNOW(props,e){
-        console.log("purchased")
-        console.log(e);
-       setPurchased ( true);
-    }
-    
-    return(
-       props.name&& <div className = "Card">
-            <img src={props.image}alt=""></img>
-            <h2>{props.name}</h2>
-            <p>{props.price}</p>
-            <p>{props.rating}</p>
-            <button onClick={()=> BUYNOW(props,event)}>BUY Now</button>
-            <p>{purchased ?"already purchased":"get it now"} </p>
-        </div>
+function Course(props) {
 
-    );
+  const [purchased, setPurchased] = useState(false);
 
+  function BUYNOW(e) {
+    console.log("purchased");
+    console.log(e);
+    setPurchased(true);
+  }
+
+  return (
+    props.name && <div className="Card">
+      <img src={props.image} alt="" />
+      <h2>{props.name}</h2>
+      <p>{props.price}</p>
+      <p>{props.rating}</p>
+      <button onClick={BUYNOW}>BUY Now</button>
+      <p>{purchased ? "already purchased" : "get it now"}</p>
+      <button onClick={() => props.delete(props.id)}>delete</button>
+    </div>
+  );
 }
-  
-Course.propType={
-    name :PropTypes.string
-    
-}
+
+Course.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  delete: PropTypes.func.isRequired
+};
+
 export default Course;
